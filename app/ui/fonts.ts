baa742f5c75c16a9440c8afd0d5d8b32bd94e856
Wesley Tsai <wes.tsai97@gmail.com>
2024-01-03 18:58:03 -0600

    invoices search bar ch 11

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,7 +1,11 @@
-import { Inter, Lusitana } from 'next/font/google';
+import { Inter, Lusitana, Press_Start_2P } from 'next/font/google';
 
 export const inter = Inter({ subsets: ['latin'] });
 export const lusitana = Lusitana({
     weight: ['400', '700'],
     subsets: ['latin'],
+});
+export const press_start = Press_Start_2P({ 
+    weight: ['400'],
+    subsets: ['latin-ext']
 });
